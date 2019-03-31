7d0ff467f91eef8231b7b83e22ece72cff0f9126
Wang-Jun-Chao <shining-glory@qq.com>
2019-03-31 08:15:13 +0800

    commit

--- spark/04-spark-the-definitive-guide/ch1301-advanced-rdd/src/main/java/wjc/bigdata/spark/advancedrdd/AdvancedRdd.java
+++ spark/04-spark-the-definitive-guide/ch1301-advanced-rdd/src/main/java/wjc/bigdata/spark/advancedrdd/AdvancedRdd.java
@@ -1,17 +1,23 @@
 package wjc.bigdata.spark.advancedrdd;
 
 import com.google.common.collect.Lists;
-import org.apache.spark.SparkConf;
+import org.apache.spark.HashPartitioner;
+import org.apache.spark.Partitioner;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import scala.Tuple2;
 import scala.util.Random;
+import wjc.bigdata.spark.util.PathUtils;
 
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,11 +30,15 @@ public class AdvancedRdd {
     private final static Logger logger = LoggerFactory.getLogger(AdvancedRdd.class);
 
     public static void main(String[] args) {
-        SparkConf conf = new SparkConf()
-                .setMaster("local[*]")
-                .setAppName("ch1301-advanced-rdd")
-                .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
-        JavaSparkContext context = new JavaSparkContext(conf);
+
+        SparkSession spark = SparkSession
+                .builder()
+                .master("local[*]")
+                .appName("ch1301-advanced-rdd")
+                .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+                .getOrCreate();
+
+        JavaSparkContext context = new JavaSparkContext(spark.sparkContext());
         String[] myCollection = "Spark The Definitive Guide : Big Data Processing Made Simple"
                 .split(" ");
         JavaRDD<String> words = context.parallelize(Lists.newArrayList(myCollection), 2);
@@ -116,6 +126,65 @@ public class AdvancedRdd {
         kvCharacters.join(keyedChars).count();
         kvCharacters.join(keyedChars, outputPartitions).count();
 
+        JavaRDD<Integer> numRange = context.parallelize(
+                Lists.newArrayList(1, 2, 3, 4, 5, 6, 7, 8, 9),
+                2);
+        words.zip(numRange).collect();
+
+        words.coalesce(1).getNumPartitions();
+
+        words.repartition(10);
+
+
+        Dataset<Row> df = spark.read()
+                .option("header", "true")
+                .option("inferSchema", "true")
+                .csv(PathUtils.workDir("../../../data/retail-data/all/"));
+        JavaRDD<Row> rdd = df.coalesce(10).toJavaRDD();
+        df.printSchema();
+
+        rdd.map(r -> r.getString(6)).take(5).forEach(System.out::println);
+        JavaPairRDD<Double, Row> keyedRDD = rdd.keyBy(row -> Double.parseDouble(row.getString(6)));
+        keyedRDD.partitionBy(new HashPartitioner(10)).take(10);
+
+        keyedRDD
+                .partitionBy(new DomainPartitioner())
+                .map(Tuple2::_1)
+                .glom()
+                .map(List::size)
+                .take(5);
+
+        context.parallelize(Lists.newArrayList(1, 2, 3, 4, 5, 6, 7, 8, 9))
+                .map(num -> new SomeClass().setSomeValue(num));
+
     }
 
+    static class SomeClass implements Serializable {
+        int someValue = 0;
+
+        public SomeClass setSomeValue(int i) {
+            this.someValue = i;
+            return this;
+        }
+    }
+
+    static class DomainPartitioner extends Partitioner {
+        int numPartitions = 3;
+
+        @Override
+        public int getPartition(Object key) {
+
+            int customerId = ((Double) key).intValue();
+            if (customerId == 17850 || customerId == 12583) {
+                return 0;
+            } else {
+                return new java.util.Random().nextInt(2) + 1;
+            }
+        }
+
+        @Override
+        public int numPartitions() {
+            return numPartitions;
+        }
+    }
 }
