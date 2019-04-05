671a2bb93a031114f5df56d4b734db5e39d52350
Wang-Jun-Chao <shining-glory@qq.com>
2019-04-05 15:24:56 +0800

    commit

--- ignite/003-database/src/main/java/wjc/spark/ignite/database/DatabaseApplication.java
+++ ignite/003-database/src/main/java/wjc/spark/ignite/database/DatabaseApplication.java
@@ -1,12 +1,16 @@
-package wjc.spark.ignite.database.controller;
+package wjc.spark.ignite.database;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * @author: wangjunchao(王俊超)
  * @time: 2019-04-05 07:29
  **/
+@SpringBootApplication
+@ImportResource(locations = {"classpath:default-config.xml"})
 public class DatabaseApplication {
     private final static Logger logger = LoggerFactory.getLogger(DatabaseApplication.class);
 }
