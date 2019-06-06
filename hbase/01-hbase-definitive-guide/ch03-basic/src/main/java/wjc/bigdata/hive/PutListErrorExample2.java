0480134e0b38b40690c5a14832a1e22a70797884
Wang-Jun-Chao <shining-glory@qq.com>
2019-06-07 06:37:34 +0800

    commit

--- hbase/01-hbase-definitive-guide/ch03-basic/src/main/java/wjc/bigdata/hive/PutListErrorExample2.java
+++ hbase/01-hbase-definitive-guide/ch03-basic/src/main/java/wjc/bigdata/hive/PutListErrorExample2.java
@@ -5,12 +5,10 @@ package wjc.bigdata.hive;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.TableName;
-import org.apache.hadoop.hbase.client.Connection;
-import org.apache.hadoop.hbase.client.ConnectionFactory;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.util.Bytes;
-import util.HBaseHelper;
+import wjc.bigdata.hbase.common.HBaseHelper;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -18,46 +16,47 @@ import java.util.List;
 
 public class PutListErrorExample2 {
 
-  public static void main(String[] args) throws IOException {
-    Configuration conf = HBaseConfiguration.create();
+    public static void main(String[] args) throws IOException {
+        Configuration conf = HBaseConfiguration.create();
 
-    HBaseHelper helper = HBaseHelper.getHelper(conf);
-    helper.dropTable("testtable");
-    helper.createTable("testtable", "colfam1");
+        try (HBaseHelper helper = HBaseHelper.getHelper(conf);) {
 
-    Connection connection = ConnectionFactory.createConnection(conf);
-    Table table = connection.getTable(TableName.valueOf("testtable"));
+            helper.dropTable("testtable");
+            helper.createTable("testtable", "colfam1");
+            Table table = helper.getTable(TableName.valueOf("testtable"));
 
-    List<Put> puts = new ArrayList<Put>();
+            List<Put> puts = new ArrayList<Put>();
 
-    // vv PutListErrorExample2
-    Put put1 = new Put(Bytes.toBytes("row1"));
-    put1.addColumn(Bytes.toBytes("colfam1"), Bytes.toBytes("qual1"),
-      Bytes.toBytes("val1"));
-    puts.add(put1);
-    Put put2 = new Put(Bytes.toBytes("row2"));
-    put2.addColumn(Bytes.toBytes("BOGUS"), Bytes.toBytes("qual1"),
-      Bytes.toBytes("val2"));
-    puts.add(put2);
-    Put put3 = new Put(Bytes.toBytes("row2"));
-    put3.addColumn(Bytes.toBytes("colfam1"), Bytes.toBytes("qual2"),
-      Bytes.toBytes("val3"));
-    puts.add(put3);
-    /*[*/
-      Put put4 = new Put(Bytes.toBytes("row2"));
-    puts.add(put4);/*]*/ // co PutListErrorExample2-1-AddErrorPut Add put with no content at all to list.
+            // vv PutListErrorExample2
+            Put put1 = new Put(Bytes.toBytes("row1"));
+            put1.addColumn(Bytes.toBytes("colfam1"), Bytes.toBytes("qual1"),
+                    Bytes.toBytes("val1"));
+            puts.add(put1);
 
-    /*[*/try {/*]*/
-      table.put(puts);
-    /*[*/} catch (Exception e) {
-      System.err.println("Error: " + e);
-      // table.flushCommits();
-      // todo: FIX!
-      /*]*/ // co PutListErrorExample2-2-Catch Catch local exception and commit queued updates.
-    /*[*/}/*]*/
-    // ^^ PutListErrorExample2
-    table.close();
-    connection.close();
-    helper.close();
-  }
+            // 列簇不存在无法插入
+//            Put put2 = new Put(Bytes.toBytes("row2"));
+//            put2.addColumn(Bytes.toBytes("BOGUS"), Bytes.toBytes("qual1"),
+//                    Bytes.toBytes("val2"));
+//            puts.add(put2);
+
+            Put put3 = new Put(Bytes.toBytes("row2"));
+            put3.addColumn(Bytes.toBytes("colfam1"), Bytes.toBytes("qual2"),
+                    Bytes.toBytes("val3"));
+            puts.add(put3);
+
+            // 没有指定列簇无法插入
+//            Put put4 = new Put(Bytes.toBytes("row2"));
+//            // co PutListErrorExample2-1-AddErrorPut Add put with no content at all to list.
+//            puts.add(put4);
+
+            try {
+                table.put(puts);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
 }
